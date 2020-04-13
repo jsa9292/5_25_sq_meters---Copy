e0336b9cf3f0c56f348c6e9ca2c9eff2bcaf8cae
Jungsu Pak <jsa9292@gmail.com>
2020-04-13 04:12:56 -0700

    update0413
    pcache sequence update
    HDRP light rebuilt

--- Assets/VFX/PcacheSqeuence.cs
+++ Assets/VFX/PcacheSqeuence.cs
@@ -2,31 +2,32 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
-using System.IO;
 
+[RequireComponent(typeof(UnityEngine.Experimental.VFX.VisualEffect))]
+[ExecuteInEditMode]
 public class PcacheSqeuence : MonoBehaviour
 {
 
     [Header("Play Setting")]
     public int index;
-    [Range(1f, 90f)]
+    [Range(0f, 10f)]
+    public float playspeed;
     public float frameRate;
-    [Range(1f, 10f)]
     public int pCountMult;
     public bool loop;
     [Header("Import Setting")]
     public string folder;
     public string file;
+    public bool checkFolder;
     public int imin = 1;
     public int imax;
     
     public List<UnityEditor.VFX.Utils.PointCacheAsset> pca;
     private UnityEngine.Experimental.VFX.VisualEffect ve;
     // Start is called before the first frame update
-    void Awake()
+    void LoadFiles()
     {
-        ve = GetComponent<UnityEngine.Experimental.VFX.VisualEffect>();
-
+        pca.Clear();
         imax = imin;
         UnityEditor.VFX.Utils.PointCacheAsset temp;
         while (temp = Resources.Load<UnityEditor.VFX.Utils.PointCacheAsset>(folder + "/" + file + imax.ToString()))
@@ -37,25 +38,39 @@ public class PcacheSqeuence : MonoBehaviour
 
             //Debug.Log(folder + "/" + file + imax.ToString() + ".pcache loaded");
         }
-        gameObject.SetActive(false);
     }
 
     // Update is called once per frame\
     private float timer;
+    private float waitTime;
     void Update()
     {
+        if (ve == null)
+        {
+            ve = GetComponent<UnityEngine.Experimental.VFX.VisualEffect>();
+        }
+        if (checkFolder)
+        {
+            LoadFiles();
+            checkFolder = false;
+        }
         if (!gameObject.activeSelf) return;
         if (index > pca.Count)
         {
             if (loop) index = imin;
             else return;
         }
-        timer += Time.deltaTime;
-        if (timer > 1 / frameRate)
+        timer += Time.deltaTime *playspeed;
+        waitTime = 1/ frameRate;
+        if (timer>= waitTime)
         {
+            timer -= waitTime;
             index++;
-            timer = 0;
+
         }
+
+
+        index = Mathf.Clamp(index, imin, imax-1);
         ve.SetFloat("pCount",pca[index].PointCount*pCountMult);
         ve.SetTexture("pos",pca[index].surfaces[0]);
         ve.SetTexture("color",pca[index].surfaces[1]);
